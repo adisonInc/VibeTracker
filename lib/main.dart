99d1ddacd97bc20e83d2051b0ef42ce5f421d880
kamil <temooo1337@gmail.com>
2020-10-11 17:38:55 +0200

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:geocoder/geocoder.dart';
+import 'package:weather/weather.dart';
 
 void main() {
   runApp(MyApp());
 }
+
+const priColor = Color(0xFF66ffad);
+const whiteColor = Colors.white;
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Code week (vibe check)',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
+        primaryColor: priColor,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -50,68 +35,104 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  String _location = "BRAK";
+  String _locationLatStr = "BRAK";
+  String _locationLongStr = "BRAK";
+  double _locationLat = 0;
+  double _locationLong = 0;
+  String _weatherStr = "BRAK";
 
-  void _incrementCounter() {
+  var _decodedCoords;
+  String address;
+
+  void _getLocation() async {
+    Position position =
+        await getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _location = "${position.latitude}, ${position.longitude}";
+      _locationLatStr = "${position.latitude}";
+      _locationLongStr = "${position.longitude}";
+      _locationLat = double.parse(_locationLatStr);
+      _locationLong = double.parse(_locationLongStr);
+    });
+    _getAdress();
+  }
+
+  void _getAdress() async {
+    final coordinates = new Coordinates(_locationLat, _locationLong);
+    _decodedCoords =
+        await Geocoder.local.findAddressesFromCoordinates(coordinates);
+    var first = _decodedCoords.first;
+    print("${first.featureName} : ${first.addressLine}");
+    setState(() {
+      address = "${first.addressLine}";
+    });
+  }
+
+  void _getWeather() async {
+    WeatherFactory wf = new WeatherFactory("045cc58f70b079bf695858508c7ea0ba",
+        language: Language.POLISH);
+    Weather w = await wf.currentWeatherByLocation(_locationLat, _locationLong);
+    setState(() {
+      _weatherStr =
+          "Temperatura Odzcuwalna: ${w.tempFeelsLike}\nTemperatura: ${w.temperature}\nZachmurzenie ${w.cloudiness}\nCiśnienie ${w.pressure}";
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          children: [
+            IconButton(
+                icon: Icon(Icons.menu), color: priColor, onPressed: () {}),
+            Spacer(),
+            IconButton(
+                icon: Icon(Icons.cloud, color: priColor),
+                onPressed: () {
+                  _getWeather();
+                }),
+            IconButton(
+                icon: Icon(Icons.location_on, color: priColor),
+                onPressed: () {
+                  _getLocation();
+                }),
+            IconButton(
+                icon: Icon(Icons.help), color: priColor, onPressed: () {}),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        child: Icon(Icons.home),
+        onPressed: () {},
+        backgroundColor: priColor,
+        elevation: 0,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      appBar: AppBar(
+        title: Text(
+          "code week (vibecheck)",
+          style: TextStyle(color: whiteColor),
+        ),
+        elevation: 0,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              '$_location',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            Text(
+              '$address',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            Text('$_weatherStr'),
+          ],
+        ),
+      ),
     );
   }
 }
